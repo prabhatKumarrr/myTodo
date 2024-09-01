1965a30713ed98c466c36a7ff421888b96c938d4
prabhatKumarrr <prabhat.swegg@gmail.com>
2024-09-01 09:22:58 +0000

    Created an HTTP server using express and ported the static myTodo web on it

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const port = process.env.PORT || 3000;
+const app = express();
+
+// route handlers
+
+app.use(express.static("staticFrontend"));
+
+app.listen(port, () => {
+  console.log("The server is running on port  --  " + port);
+})
